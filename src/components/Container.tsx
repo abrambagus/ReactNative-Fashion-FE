6df1dec4e921a966e3dbb6cbed38c7eedf2843d4
abrambagus <you@example.com>
2022-01-28 21:13:52 +0700

    footer-fix

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,13 @@
 import React, { ReactNode } from "react";
-import { Dimensions, Image, StatusBar, StyleSheet } from "react-native";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  ScrollView,
+  StatusBar,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import theme, { Box } from "./Theme";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 interface ContainerProps {
   children: ReactNode;
@@ -11,7 +16,7 @@ interface ContainerProps {
 
 export const assets = [require("../../assets/images/patterns/1.png")];
 
-const { width } = Dimensions.get("window");
+const { width, height: windowHeight } = Dimensions.get("window");
 const aspectRatio = 750 / 1125;
 const height = width * aspectRatio;
 
@@ -19,48 +24,50 @@ const Container = ({ children, footer }: ContainerProps) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <Box flex={1} backgroundColor="secondary">
-      <StatusBar barStyle="light-content" />
-      <Box backgroundColor="white">
-        <Box
-          borderBottomLeftRadius="xl"
-          overflow="hidden"
-          height={height * 0.61}
-        >
+    <ScrollView>
+      <Box flex={1} height={windowHeight + 33} backgroundColor="secondary">
+        <StatusBar barStyle="light-content" />
+        <Box backgroundColor="white">
+          <Box
+            borderBottomLeftRadius="xl"
+            overflow="hidden"
+            height={height * 0.61}
+          >
+            <Image
+              source={assets[0]}
+              style={{
+                width,
+                height,
+                borderBottomLeftRadius: theme.borderRadii.xl,
+              }}
+            />
+          </Box>
+        </Box>
+        <Box flex={1} overflow="hidden">
           <Image
             source={assets[0]}
             style={{
+              ...StyleSheet.absoluteFillObject,
               width,
               height,
-              borderBottomLeftRadius: theme.borderRadii.xl,
+              top: -height * 0.61,
             }}
           />
+          <Box
+            borderRadius="xl"
+            borderTopLeftRadius="n"
+            backgroundColor="white"
+            flex={1}
+          >
+            {children}
+          </Box>
+        </Box>
+        <Box backgroundColor="secondary" paddingTop="m">
+          {footer}
+          <Box height={insets.bottom} />
         </Box>
       </Box>
-      <Box flex={1} overflow="hidden">
-        <Image
-          source={assets[0]}
-          style={{
-            ...StyleSheet.absoluteFillObject,
-            width,
-            height,
-            top: -height * 0.61,
-          }}
-        />
-        <Box
-          borderRadius="xl"
-          borderTopLeftRadius="n"
-          backgroundColor="white"
-          flex={1}
-        >
-          {children}
-        </Box>
-      </Box>
-      <Box backgroundColor="secondary" paddingTop="m">
-        {footer}
-        <Box height={insets.bottom} />
-      </Box>
-    </Box>
+    </ScrollView>
   );
 };
 
