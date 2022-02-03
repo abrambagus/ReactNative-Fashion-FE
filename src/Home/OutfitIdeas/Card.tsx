885bf9dcdbc84df85bcb407f520aa583ae9dcc4a
abrambagus <you@example.com>
2022-02-03 19:24:23 +0700

    swipe-animations-done

--- src/Home/OutfitIdeas/Card.tsx
+++ src/Home/OutfitIdeas/Card.tsx
@@ -1,17 +1,20 @@
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, ImageRequireSource, StyleSheet } from "react-native";
 import React from "react";
 import { Box } from "../../components";
-import Animated, { add } from "react-native-reanimated";
-import {
-  mix,
-  mixColor,
-  usePanGestureHandler,
-  withSpring,
-} from "react-native-redash";
+import { useSpring } from "./Animations";
+import Animated, {
+  add,
+  interpolate,
+  Extrapolate,
+} from "react-native-reanimated";
+import { mixColor, mix, usePanGestureHandler } from "react-native-redash";
 import { PanGestureHandler } from "react-native-gesture-handler";
 
 interface CardProps {
-  position: Animated.Adaptable<number>;
+  position: Animated.Node<number>;
+  onSwipe: () => void;
+  source: ImageRequireSource;
+  step: number;
 }
 
 const { width: wWidth } = Dimensions.get("window");
@@ -19,21 +22,27 @@ const width = wWidth * 0.7;
 const height = width * (425 / 294);
 const borderRadius = 24;
 
-const Card = ({ position }: CardProps) => {
+const Card = ({ position, onSwipe, source, step }: CardProps) => {
   const { gestureHandler, translation, velocity, state } =
     usePanGestureHandler();
   const backgroundColor = mixColor(position, "#C9E9E7", "#74BCB8");
   const translateYOffset = mix(position, 0, -50);
   const scale = mix(position, 1, 0.9);
-  const translateX = withSpring({
+  const imageScale = interpolate(position, {
+    inputRange: [0, step],
+    outputRange: [1.2, 1],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const translateX = useSpring({
     value: translation.x,
     velocity: velocity.x,
     state,
-    snapPoints: [-width, 0, width],
+    snapPoints: [-wWidth, 0, wWidth],
+    onSnap: ([x]) => x !== 0 && onSwipe(),
   });
   const translateY = add(
     translateYOffset,
-    withSpring({
+    useSpring({
       value: translation.y,
       velocity: velocity.y,
       state,
@@ -55,8 +64,19 @@ const Card = ({ position }: CardProps) => {
             height,
             borderRadius,
             transform: [{ translateY }, { translateX }, { scale }],
+            overflow: "hidden",
           }}
-        />
+        >
+          <Animated.Image
+            {...{ source }}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              width: undefined,
+              height: undefined,
+              transform: [{ scale: imageScale }],
+            }}
+          />
+        </Animated.View>
       </PanGestureHandler>
     </Box>
   );
--- src/Home/OutfitIdeas/OutfitIdeas.tsx
+++ src/Home/OutfitIdeas/OutfitIdeas.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { sub } from "react-native-reanimated";
+import { useTransition } from "react-native-redash";
 import { Box, Header } from "../../components";
 import { HomeNavigationProps } from "../../components/Naviagtion";
 import Background from "./Background";
 import Card from "./Card";
 
+const cards = [
+  {
+    index: 3,
+    source: require("../../../assets/images/4.png"),
+  },
+  {
+    index: 2,
+    source: require("../../../assets/images/3.png"),
+  },
+  {
+    index: 1,
+    source: require("../../../assets/images/2.png"),
+  },
+  {
+    index: 0,
+    source: require("../../../assets/images/1.png"),
+  },
+];
+
+const step = 1 / (cards.length - 1);
+
 const OutfitIdeas = ({ navigation }: HomeNavigationProps<"OutfitIdeas">) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const aIndex = useTransition(currentIndex);
+  console.log(currentIndex);
   return (
     <Box flex={1} backgroundColor="white">
       <Header
@@ -14,9 +40,17 @@ const OutfitIdeas = ({ navigation }: HomeNavigationProps<"OutfitIdeas">) => {
       />
       <Box flex={1}>
         <Background />
-        <Card position={1} />
-        <Card position={0.5} />
-        <Card position={0} />
+        {cards.map(
+          ({ index, source }) =>
+            currentIndex < index * step + step && (
+              <Card
+                key={index}
+                position={sub(index * step, aIndex)}
+                onSwipe={() => setCurrentIndex((prev) => prev + step)}
+                {...{ source, step }}
+              />
+            )
+        )}
       </Box>
     </Box>
   );
