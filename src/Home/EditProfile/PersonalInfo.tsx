b94f0a0e726081c6c80536387b1ba2f5ace110c4
abrambagus <you@example.com>
2022-02-09 15:29:05 +0700

    edit-profile-done

--- src/Home/EditProfile/PersonalInfo.tsx
+++ src/Home/EditProfile/PersonalInfo.tsx
@@ -1,11 +1,55 @@
 import React from "react";
+import { ScrollView } from "react-native";
 import { Box, Text } from "../../components";
+import TextInput from "../../components/Form/TextInput";
+import CheckboxGroup from "./CheckboxGroup";
+
+const genders = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+];
 
 const PersonalInfo = () => {
   return (
-    <Box>
-      <Text>Personal Info</Text>
-    </Box>
+    <ScrollView>
+      <Box padding="m">
+        <Text variant="body" marginBottom="m">
+          Account Information
+        </Text>
+        <Box marginBottom="m">
+          <TextInput
+            icon="user"
+            placeholder="Name"
+            autoCapitalize="none"
+            autoCompleteType="name"
+            returnKeyType="next"
+            returnKeyLabel="next"
+          />
+        </Box>
+        <Box marginBottom="m">
+          <TextInput
+            icon="lock"
+            placeholder="Enter your password"
+            autoCompleteType="password"
+            autoCapitalize="none"
+            returnKeyType="next"
+            returnKeyLabel="next"
+            secureTextEntry
+          />
+        </Box>
+        <Box marginBottom="m">
+          <TextInput
+            icon="map-pin"
+            placeholder="Address"
+            autoCapitalize="none"
+            autoCompleteType="street-address"
+            returnKeyType="go"
+            returnKeyLabel="go"
+          />
+        </Box>
+        <CheckboxGroup options={genders} radio />
+      </Box>
+    </ScrollView>
   );
 };
 
